32c9f089355b8dd9618e192e60b07385f1bf833f
N2630 <n.teychon@gmail.com>
2025-03-12 21:24:33 +0100

    Ajout de fichiers de configuration et d'exportation pour plusieurs modules, ainsi que des fichiers de test et d'exemples.

---
+++ connectDb.js
@@ -0,0 +1,25 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+
+const uri = process.env.MONGODB_URI
+const dbClient = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+})
+
+async function connectToDatabase() {
+  try {
+    await dbClient.connect();
+    console.log('Connecté à MongoDB');
+  } catch (error) {
+    console.error('Erreur de connexion à MongoDB', error);
+  }
+}
+
+module.exports = {
+    dbClient,
+    connectToDatabase
+}
---
+++ index.js
@@ -0,0 +1,156 @@
+const axios = require("axios");
+const { parse } = require("path");
+
+
+
+async function getEDTInfo(date) {
+    try {
+        const weekNumber = await getWeekNumber(date);
+        const yearNumber = await getYearNumber(date)
+        const trainProg = "BUT1"
+        const group = "3B"
+        const response = await axios.get(`https://flopedt.iut-blagnac.fr/fr/api/fetch/scheduledcourses/?week=${weekNumber}&year=${yearNumber}&dept=INFO&train_prog=${trainProg}&group=${group}&lineage=true`)
+        //let responseData = []
+
+        const dayInitial = getDayInitials(date);
+        //console.log(response)
+        //console.log(dayInitial)
+        let responseData = []
+        responseData = response.data.filter(cour => cour.day === dayInitial.ref)
+        
+        const formatTime = (minutes) => {
+            const hours = Math.floor(minutes / 60);
+            const mins = minutes % 60;
+            return `${hours}h${mins.toString().padStart(2, '0')}`;
+        }
+        await console.log(responseData)
+        await responseData.forEach(cour => {
+            console.log(`Module: ${cour.course.module.name}, Start time: ${formatTime(cour.start_time)}`);
+        });
+        
+        return responseData
+    } catch (err) {
+        console.error("Erreur lors de la récupération des données :", err)
+    }
+}
+
+async function getFreeRoom(date) {
+    const weekNumber = await getWeekNumber(date);
+    const yearNumber = await getYearNumber(date);
+    const response = await axios.get(
+        `https://flopedt.iut-blagnac.fr/fr/api/fetch/scheduledcourses/?week=${weekNumber}&year=${yearNumber}&dept=INFO`
+    );
+
+    const dayInitial = getDayInitials(date);
+
+    const rooms = [
+        { ref: "24", id: 24, name: "B101" },
+        { ref: "25", id: 25, name: "B102" },
+        { ref: "26", id: 26, name: "B103" },
+        { ref: "27", id: 27, name: "B104" },
+        { ref: "28", id: 28, name: "B105" },
+        { ref: "29", id: 29, name: "B106" },
+        { ref: "30", id: 30, name: "B004" },
+        { ref: "32", id: 32, name: "B007" },
+        { ref: "33", id: 33, name: "B008" },
+        { ref: "34", id: 34, name: "B009" },
+        { ref: "37", id: 37, name: "B219" },
+ 
+    ];
+
+    const coursTime = [
+        { id: 1, heure: "8h00", minTime: 480 },
+        { id: 2, heure: "9h30", minTime: 570 },
+        { id: 3, heure: "11h05", minTime: 665 },
+        { id: 4, heure: "14h15", minTime: 855 },
+        { id: 5, heure: "15h45", minTime: 945 },
+        { id: 6, heure: "17h10", minTime: 1030 },
+
+    ];
+
+    const curruentTime = getCurrentTimeInMinutes();
+
+    // Trouver le cours en cours et le prochain
+    const currentCours = getMin(coursTime, curruentTime)
+    const nextCours = coursTime.find(cours => cours.minTime >= curruentTime);
+
+
+    let timeToFocus = 0;
+
+    if (nextCours && (nextCours.minTime - curruentTime) > 15) {
+        timeToFocus = currentCours.minTime;
+        
+    } else if (nextCours) {
+        timeToFocus = nextCours.minTime;
+    } else {
+        return
+    }
+
+    let freeRoom = [];
+
+    for (const room of rooms) {
+        // Vérifie si la salle est utilisée dans les données de l'API
+        const isOccupied = response.data.some(
+            (course) => course.room && course.room.id === room.id && course.start_time === timeToFocus && course.day === dayInitial.ref
+        );
+
+        if (!isOccupied) {
+            freeRoom.push(room.name);
+        }
+    }
+
+    /*if (freeRoom.length === 0) {
+        console.log("Aucune salle libre trouvée.");
+    } else {
+        console.log("Salles libres : ", freeRoom);
+    }*/
+    
+    return freeRoom;
+}
+
+
+async function getWeekNumber(date) {
+    const firstDayOfYear = new Date(date.getFullYear(), 0, 1);
+    const pastDaysOfYear = (date - firstDayOfYear) / 86400000;
+    return Math.ceil((pastDaysOfYear + firstDayOfYear.getDay() + 1) / 7);
+}
+
+async function getYearNumber(date) {
+    return date.getFullYear(); // retourne l'année
+}
+
+const getDayInitials = (date) => {
+    const days = [
+        { ref: "m", name: "Lun." }, // lundi
+        { ref: "tu", name: "Mar." }, // mardi
+        { ref: "w", name: "Mer." }, // mercredi
+        { ref: "th", name: "Jeu." }, // jeudi
+        { ref: "f", name: "Ven." }, // vendredi
+        { ref: "s", name: "Sam." }, // samedi
+        { ref: "su", name: "Dim." } // dimanche
+    ];
+
+    const daysToReturn = days[date.getDay() - 1]
+    //console.log(daysToReturn)
+    return daysToReturn; // `getDay()` renvoie un numéro de 0 (dimanche) à 6 (samedi)
+
+}
+
+function getCurrentTimeInMinutes() {
+    const now = new Date(); // Obtenir la date et l'heure actuelles
+    const hours = now.getHours(); // Obtenir les heures (0-23)
+    const minutes = now.getMinutes(); // Obtenir les minutes (0-59)
+
+    return hours * 60 + minutes; // Convertir en minutes depuis minuit
+}
+
+function getMin(coursTime, curruentTime){
+    let min = 1030
+
+    for (const cour of coursTime) {
+        if(cour.minTime < curruentTime ){
+            min = cour
+        }
+    }
+    return min
+}
