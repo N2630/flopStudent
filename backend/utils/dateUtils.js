d552045601713ef51d62fc3109006031624cc1d6
N2630 <n.teychon@gmail.com>
2025-09-02 14:39:59 +0200

    correction dateUtils

--- backend/utils/dateUtils.js
+++ backend/utils/dateUtils.js
@@ -4,9 +4,10 @@ function getWeekAndYear(dateToAnalyse) {
     const date = dateToAnalyse ? new Date(dateToAnalyse) : new Date();
     date.setHours(0,0,0,0);
     date.setDate(date.getDate() + 3 - (date.getDay() + 6) % 7);
-    const week = Number('0' + (1 + Math.round(((date.getTime() - week.getTime()) / 86400000 - 3 + (week.getDay() + 6) % 7) / 7))).slice(-2);
+    const week = new Date(date.getFullYear(), 0,4);
+    const weekToReturn = Number('0' + (1 + Math.round(((date.getTime() - week.getTime()) / 86400000 - 3 + (week.getDay() + 6) % 7) / 7))).slice(-2);
     const year = date.getFullYear();
-    return { week, year };
+    return { week: weekToReturn, year };
 }
 
 module.exports = {
